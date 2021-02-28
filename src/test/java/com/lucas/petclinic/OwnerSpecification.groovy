fa448aedcfdbb2b0a071fe5be6cecc8245ec9774
Matthew Lucas <lucasmatthew@hotmail.com>
2021-02-28 21:06:40 +0000

    Added pause to container startup

--- src/test/java/com/lucas/petclinic/OwnerSpecification.groovy
+++ src/test/java/com/lucas/petclinic/OwnerSpecification.groovy
@@ -2,6 +2,8 @@ package com.lucas.petclinic
 
 import org.junit.ClassRule
 import org.testcontainers.containers.DockerComposeContainer
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.containers.wait.strategy.WaitStrategy
 import org.testcontainers.spock.Testcontainers
 import spock.lang.Specification
 
@@ -10,12 +12,12 @@ class OwnerSpecification extends Specification {
 
     @ClassRule
     public DockerComposeContainer environment = new DockerComposeContainer(new File("./docker-compose.yml"))
-        .withExposedService("petclinic", 9966)
+        .withExposedService("petclinic", 9966);
 
     def "creates an owner"() {
 
         when: "an owner is created"
-        environment.getServicePort("petclinic", 9966)
+        environment.getContainerByServiceName("petclinic").get().getMappedPort(9966)
 
         then: "that owner can be seen in the list of owners"
 
